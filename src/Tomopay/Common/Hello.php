4eaefe4b68cd1ad4e00707908bbe1b7be4f13836
masakiNaito <masaki.naito@rakus.co.jp>
2019-10-09 11:24:51 +0900

    first commit

---
+++ src/Tomopay/Common/Hello.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tomopay\Common;
+
+/**
+ * パッケージインストール動作確認用クラス
+ */
+class Hello
+{
+    /**
+     * 挨拶をする
+     *
+     * @return string
+     */
+    public static function sayHello()
+    {
+        return "Hello world";
+    }
+}
